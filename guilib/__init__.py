20bafd1c1b69130a4cd7b35c16e5e9ed669d11d4
Igor Omelchenko <counter3d@gmail.com>
2017-04-24 14:38:04 +0300

    Удалена папка guilib

--- guilib/__init__.py
+++
@@ -1,7 +0,0 @@
-import PyQt5
-from PyQt5 import QtCore
-
-print('PyQt5 version:', QtCore.PYQT_VERSION_STR)
-print('Qt5 version:', QtCore.QT_VERSION_STR)
-
-
--- travianbot/model/overwatch.py
+++ travianbot/model/overwatch.py
@@ -1,6 +1,4 @@
-import time
-
-from guilib import push
+from .web import push
 
 
 class Overwatch:
