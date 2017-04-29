e8be3de3ac21b6a71f4f7427b9b684a7b1b430a7
Igor <counter3d@gmail.com>
2017-04-29 13:23:10 +0300

    Налажена система сборки программы через cx_freeze

--- setup.py
+++ setup.py
@@ -2,17 +2,37 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = [])
+product_name = 'Travian Legends Bot'
+bdist_msi_options = {
+    'upgrade_code': '{66620F3A-DC3A-11E2-B341-002219E9B01E}',
+    'add_to_path': False,
+    'initial_target_dir': r'[ProgramFilesFolder]\%s' % (product_name),
+}
+
+include_files = ['config.json', 'data']
+
+buildOptions = dict(
+    packages=['travianapi', 'travianbot'],
+    excludes=[],
+    includes=['travianapi', 'travianbot', 'queue'],
+    include_files=include_files)
 
 import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+targetName = 'travianbot'
+if sys.platform == 'win32':
+    targetName += '.exe'
 
 executables = [
-    Executable('bot_gui_qt5.py', base=base, targetName = 'travianbot')
+    Executable('bot_gui_qt5.py',
+               base=base,
+               targetName=targetName,
+               icon="data/images/icon.ico")
 ]
 
 setup(name='Travian Legends Bot',
-      version = '0.1.0',
-      description = 'Bot for Travian Legends Browser Game',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+      version='0.1.0',
+      description='Bot for Travian Legends Browser Game',
+      options=dict(bdist_msi=bdist_msi_options, build_exe=buildOptions),
+      executables=executables)
